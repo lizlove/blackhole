e4e112dd09ccc081447f617508820bacf9162ddc
lizlove <elizabethlovero@gmail.com>
2021-06-12 21:46:10 -0400

    Add basic UA animation.

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -1,88 +1,96 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
-const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
 
 module.exports = {
-    entry: path.resolve(__dirname, '../src/script.js'),
-    output:
+  entry: path.resolve(__dirname, '../src/script.js'),
+  output:
     {
-        filename: 'bundle.[contenthash].js',
-        path: path.resolve(__dirname, '../dist')
+      filename: 'bundle.[contenthash].js',
+      path: path.resolve(__dirname, '../dist'),
     },
-    devtool: 'source-map',
-    plugins:
+  devtool: 'source-map',
+  plugins:
     [
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: path.resolve(__dirname, '../static') }
-            ]
-        }),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '../src/index.html'),
-            minify: true
-        }),
-        new MiniCSSExtractPlugin()
+      new CopyWebpackPlugin({
+        patterns: [
+          {from: path.resolve(__dirname, '../static')},
+        ],
+      }),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, '../src/index.html'),
+        minify: true,
+      }),
+      new MiniCSSExtractPlugin(),
     ],
-    module:
+  module:
     {
-        rules:
+      rules:
         [
-            // HTML
-            {
-                test: /\.(html)$/,
-                use: ['html-loader']
-            },
+          // Shaders
+          {
+            test: /\.(glsl|vs|fs|vert|frag)$/,
+            exclude: /node_modules/,
+            use: [
+              'raw-loader',
+            ],
+          },
+          // HTML
+          {
+            test: /\.(html)$/,
+            use: ['html-loader'],
+          },
 
-            // JS
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use:
+          // JS
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use:
                 [
-                    'babel-loader'
-                ]
-            },
+                  'babel-loader',
+                ],
+          },
 
-            // CSS
-            {
-                test: /\.css$/,
-                use:
+          // CSS
+          {
+            test: /\.css$/,
+            use:
                 [
-                    MiniCSSExtractPlugin.loader,
-                    'css-loader'
-                ]
-            },
+                  MiniCSSExtractPlugin.loader,
+                  'css-loader',
+                ],
+          },
 
-            // Images
-            {
-                test: /\.(jpg|png|gif|svg)$/,
-                use:
+          // Images
+          {
+            test: /\.(jpg|png|gif|svg)$/,
+            use:
                 [
-                    {
-                        loader: 'file-loader',
-                        options:
+                  {
+                    loader: 'file-loader',
+                    options:
                         {
-                            outputPath: 'assets/images/'
-                        }
-                    }
-                ]
-            },
+                          outputPath: 'assets/images/',
+                        },
+                  },
+                ],
+          },
 
-            // Fonts
-            {
-                test: /\.(ttf|eot|woff|woff2)$/,
-                use:
+          // Fonts
+          {
+            test: /\.(ttf|eot|woff|woff2)$/,
+            use:
                 [
-                    {
-                        loader: 'file-loader',
-                        options:
+                  {
+                    loader: 'file-loader',
+                    options:
                         {
-                            outputPath: 'assets/fonts/'
-                        }
-                    }
-                ]
-            }
-        ]
-    }
-}
+                          outputPath: 'assets/fonts/',
+                        },
+                  },
+                ],
+          },
+        ],
+    },
+};
